45061918d0be61a8e8be455090bba5bcc4a7b27e
Evandro Calado <evandrocalado07@gmail.com>
2024-05-08 21:50:26 -0300

    🐳 chore: route create user

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,8 @@
-import { Request, Response, Router } from "express";
-import { CustomError } from "./server";
+import { Router } from "express";
+import { CreateUserController } from "./controllers";
 
 const router = Router();
 
-router.get("/user", (req: Request, res: Response) => {
-  throw new CustomError("Something went wrong", 400);
-});
+router.post("/user", CreateUserController.handle);
 
 export default router;
