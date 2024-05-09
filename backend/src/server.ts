7d75e9af48253fa26c80856f6ca14b35b834657c
Evandro Calado <evandrocalado07@gmail.com>
2024-05-08 21:53:36 -0300

    🐳 chore: create middleware error

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,39 +1,29 @@
 import cors from "cors";
-import express, { NextFunction, Request, Response } from "express";
+import express, {
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
 import router from "./routes";
 
 const app = express();
 
-export class CustomError extends Error {
-  statusCode: number;
-
-  constructor(message: string, statusCode: number) {
-    super(message);
-    this.statusCode = statusCode;
-  }
-}
-
-const errorhandler = (
-  err: CustomError,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  if (res.headersSent) {
-    return next(err);
-  }
-
-  const statusCode = err.statusCode || 500;
-
-  const message = err.message || "Internal Server Error";
-
-  res.status(statusCode).json({ error: message });
-};
-
 app.use(express.json());
-app.use(router);
 app.use(cors());
 
-app.use(errorhandler);
+app.use(router);
 
-app.listen(3333, () => console.log("Listening on port 3000"));
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof Error) {
+    return res.status(400).json({
+      message: error.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+  });
+});
+
+app.listen(3333, () => console.log("Listening on port 3333"));
