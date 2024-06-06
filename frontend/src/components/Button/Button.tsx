6280988ee8db6b7e706311ff260e944ca6a58ce7
Evandro Calado <evandrocalado07@gmail.com>
2024-06-06 14:07:54 -0300

    🌈 style: remove margin

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -10,7 +10,7 @@ export const Button: FC<ButtonProps> = ({ children, ...rest }) => {
     <button
       {...rest}
       type="submit"
-      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none"
+      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none"
     >
       {children}
     </button>
